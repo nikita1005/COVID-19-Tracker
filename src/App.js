4d8e7a39ac28443c10c92c0bff3e5d1977ee0edf
Nikita Mahdole <nikitamahdole@gmail.com>
2020-07-30 20:15:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+below is a best to import multiple folder*/
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+    }
+
+render(){
+    return(
+        <div className={styles.container}>
+            <Cards/>
+            <CountryPicker/>
+            <Chart/>
+        </div>
+    )
 }
-
+}
 export default App;
